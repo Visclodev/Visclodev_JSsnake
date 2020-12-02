ae5d43d99249d1f6bea23258b65d458eafe885b9
VISCLO <Yobobis@gmail.com>
2020-12-02 12:39:12 +0100

    quick update! Snake can move in specific directions under circumstances

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Visclodev_JSsnake
 A snake game in JS made by Visclodev
+made with the tutorial of @WebDevSimplified:
+https://youtu.be/QTcIXok9wNY
--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED} from './snake.js'
+let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board")
+
 function main(currentTime)
 {
     window.requestAnimationFrame(main);
-    console.log(currentTime);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED)
+        return;
+    lastRenderTime = currentTime;
+    console.log("Render");
+    update();
+    draw();
 }
 
 window.requestAnimationFrame(main);
+
+function update()
+{
+    updateSnake();
+}
+
+function draw()
+{
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+}
